776c56796f30ba135f5cbd38ec07240236201746
Balazs M. Fekete <bfekete@ccny.cuny.edu>
2019-05-03 22:47:13 -0400

    Testing the completion of the new thread after all threads started.

--- CMlib/src/cmThreads.c
+++ CMlib/src/cmThreads.c
@@ -307,8 +307,9 @@ CMthreadTeam_p CMthreadTeamInitialize (CMthreadTeam_p team, size_t threadNum) {
                 free(team);
                 return ((CMthreadTeam_p) NULL);
             }
-            while (pthread_kill(team->Threads[threadId].Thread,0) != 0); // TODO this might turn out to be sloppy
         }
+        for (threadId = 0; threadId < team->ThreadNum; ++threadId)
+            while (pthread_kill(team->Threads[threadId].Thread,0) != 0); // TODO this might turn out to be sloppy
         pthread_attr_destroy(&thread_attr);
         pthread_mutex_lock (&(team->MMutex));
     }
