7ad4bbccfab321bcc5add40c872adfa9408a983e
Balazs M. Fekete <bfekete@ccny.cuny.edu>
2019-08-01 18:47:41 -0400

    Reverting back to serial mode.
    I forgott that the SERIEL mode is necessary for appending tables.

--- RGlib/src/RGlibTable.C
+++ RGlib/src/RGlibTable.C
@@ -124,7 +124,7 @@ DBInt RGlibTableToSQL (DBObjTable *table, const char *dbSchemaName, const char *
             nameLength = nameLength > strlen (record->Name ()) ? nameLength : strlen (record->Name ());
         }
         fprintf (outFile,"(\n");
-        fprintf (outFile,"\"%s\" INTEGER,\n",_RGlibSQLCaseChange (sqlCase, "ID", bufferPtr, bufferLen));
+        fprintf (outFile,"\"%s\" SERIAL,\n",_RGlibSQLCaseChange (sqlCase, "ID", bufferPtr, bufferLen));
         fprintf (outFile,"\"%s\" CHARACTER VARYING(%d) COLLATE pg_catalog.\"default\",\n",_RGlibSQLCaseChange (sqlCase, "RecordName", bufferPtr, bufferLen),nameLength + 1);
         for (field = fields->First(); field != (DBObjTableField *) NULL; field = fields->Next()) {
             if (DBTableFieldIsVisible (field))
@@ -153,15 +153,14 @@ DBInt RGlibTableToSQL (DBObjTable *table, const char *dbSchemaName, const char *
     if ((RGlibTableCopy == mode) || (RGlibTableAppend == mode)) {
         if (dbSchemaName == (char *) NULL) fprintf (outFile,"INSERT INTO  \"%s\" (",  _RGlibSQLCaseChange (sqlCase, dbTableName, bufferPtr, bufferLen));
         else fprintf (outFile,"\nINSERT INTO  \"%s\".\"%s\" (", _RGlibSQLCaseChange (sqlCase, dbSchemaName, bufferPtr, bufferLen),  _RGlibSQLCaseChange (sqlCase, dbTableName, bufferPtr + 1, bufferLen + 1));
-        fprintf (outFile, "\"%s\"",_RGlibSQLCaseChange (sqlCase, "ID",         bufferPtr, bufferLen));
-        fprintf (outFile,",\"%s\"",_RGlibSQLCaseChange (sqlCase, "RecordName", bufferPtr, bufferLen));
+        fprintf (outFile,"\"%s\"",_RGlibSQLCaseChange (sqlCase, "RecordName", bufferPtr, bufferLen));
         for (field = fields->First(); field != (DBObjTableField *) NULL; field = fields->Next()) {
             if (DBTableFieldIsVisible (field)) fprintf (outFile,",\"%s\"",_RGlibSQLCaseChange (sqlCase, field->Name(), bufferPtr, bufferLen));
         }
         fprintf (outFile,") VALUES\n");
         for (record = table->First (); record != (DBObjRecord *) NULL; record = table->Next ()) {
-            if (record->RowID () == 0) fprintf (outFile,    "(%d,$$%s$$", record->RowID () + 1, record->Name());
-            else                       fprintf (outFile,"),\n(%d,$$%s$$", record->RowID () + 1, record->Name());
+            if (record->RowID () == 0) fprintf (outFile,    "($$%s$$", record->Name());
+            else                       fprintf (outFile,"),\n($$%s$$", record->Name());
             for (field = fields->First(); field != (DBObjTableField *) NULL; field = fields->Next()) {
                 if (DBTableFieldIsVisible (field))
                     switch (field->Type()) {
