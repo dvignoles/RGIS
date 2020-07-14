6f9f95e412458f99f2d0585c252346b9f2902036
Balazs M. Fekete <bfekete@ccny.cuny.edu>
2020-07-14 12:49:06 -0400

    Continued unification.

--- WBMstable/src/MDWTempRiver.c
+++ WBMstable/src/MDWTempRiver.c
@@ -18,38 +18,29 @@ Calculate the temperature in runoff from the local grid cell.  Weight groundwate
 // Input
 static int _MDInSurfRunoffID          = MFUnset;
 static int _MDInBaseFlowID            = MFUnset;
+static int _MDInWTempSurfRunoffID     = MFUnset;
 static int _MDInWTempGrdWaterID       = MFUnset;
-static int _MDInWTempSurfRunoffPoolID = MFUnset;		// RJS 060512
-static int _MDInTotalSurfRunoffID     = MFUnset;		// RJS 082812
 
 // Output
 static int _MDOutWTempRiverID     = MFUnset;
 
 static void _MDWTempRiver (int itemID) {
-	 float RechargeT;
-	 float GrdWaterT;
-//	 float SurfaceRO;		// comment out 082812
-	 float TotalSurfRunoff;		// RJS 082812
-	 float GrdWaterRO;
-	 float TemperatureRO;
+	float RechargeT;
+	float GrdWaterT;
+	float SurfaceRO;
+	float GrdWaterRO;
+	float TemperatureRO;
 
-	 RechargeT          = MFVarGetFloat (_MDInWTempSurfRunoffPoolID, itemID, 0.0);			// RJS 060512
-  	 GrdWaterT          = MFVarGetFloat (_MDInWTempGrdWaterID,       itemID, 0.0);
-//   	 SurfaceRO          = MFVarGetFloat (_MDInSurfRunoffID,          itemID, 0.0);			// comment out 082812
-	 TotalSurfRunoff    = MFVarGetFloat (_MDInTotalSurfRunoffID,     itemID, 0.0);			// RJS 082812
- 	 GrdWaterRO         = MFVarGetFloat (_MDInBaseFlowID,            itemID, 0.0);
- 
-//      SurfaceRO  = MDMaximum(0, SurfaceRO);								// comment out 082812
-        GrdWaterRO = MDMaximum(0, GrdWaterRO);
-	TotalSurfRunoff = MDMaximum(0, TotalSurfRunoff);						// RJS 082812
-                
-//   	TemperatureRO = MDMaximum((((SurfaceRO * RechargeT) + (GrdWaterRO * GrdWaterT)) / (SurfaceRO + GrdWaterRO)),0.0);		// commented out 082812
-	TemperatureRO = MDMaximum((((TotalSurfRunoff * RechargeT) + (GrdWaterRO * GrdWaterT)) / (TotalSurfRunoff + GrdWaterRO)),0.0);	// RJS 082812
-   	
-   	
-	MFVarSetFloat(_MDOutWTempRiverID,itemID,TemperatureRO); 
+	SurfaceRO          = MFVarGetFloat (_MDInSurfRunoffID,          itemID, 0.0);
+	GrdWaterRO         = MFVarGetFloat (_MDInBaseFlowID,            itemID, 0.0);
+	RechargeT          = MFVarGetFloat (_MDInWTempSurfRunoffID,     itemID, 0.0);
+	GrdWaterT          = MFVarGetFloat (_MDInWTempGrdWaterID,       itemID, 0.0);
 
+	SurfaceRO  = MDMaximum(0, SurfaceRO);
+	GrdWaterRO = MDMaximum(0, GrdWaterRO);
 
+	TemperatureRO = MDMaximum((((SurfaceRO * RechargeT) + (GrdWaterRO * GrdWaterT)) / (SurfaceRO + GrdWaterRO)),0.0);
+	MFVarSetFloat(_MDOutWTempRiverID,itemID,TemperatureRO);
 }
 
 int MDWTempRiverDef () {
@@ -58,13 +49,11 @@ int MDWTempRiverDef () {
 
 	MFDefEntering ("River temperature");
 
-	if (((_MDInSurfRunoffID      = MDRainSurfRunoffDef ()) == CMfailed) ||
-	    ((_MDInBaseFlowID        = MDBaseFlowDef       ()) == CMfailed) ||
-//	    ((_MDInWTempSurfRunoffID = MDWTempSurfRunoffDef ()) == CMfailed) ||		// commented out RJS 060512
-	    ((_MDInWTempSurfRunoffPoolID = MDWTempSurfRunoffPoolDef ()) == CMfailed) ||		// RJS 060512
+	if (((_MDInSurfRunoffID      = MDRainSurfRunoffDef  ()) == CMfailed) ||
+	    ((_MDInBaseFlowID        = MDBaseFlowDef        ()) == CMfailed) ||
+	    ((_MDInWTempSurfRunoffID = MDWTempSurfRunoffDef ()) == CMfailed) ||
 	    ((_MDInWTempGrdWaterID   = MDWTempGrdWaterDef   ()) == CMfailed) ||
-	    ((_MDInTotalSurfRunoffID = MFVarGetID (MDVarTotalSurfRunoff, "mm",  MFInput,  MFFlux, MFBoundary)) == CMfailed) ||	//RJS 082812
-	    ((_MDOutWTempRiverID     = MFVarGetID (MDVarWTempRiver,    "degC", MFOutput, MFState, MFBoundary)) == CMfailed) ||	
+	    ((_MDOutWTempRiverID     = MFVarGetID (MDVarWTempRiver, "degC", MFOutput, MFState, MFBoundary)) == CMfailed) ||	
 	    (MFModelAddFunction (_MDWTempRiver) == CMfailed)) return (CMfailed);
 
 	MFDefLeaving ("River temperature");
