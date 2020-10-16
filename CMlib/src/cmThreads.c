1a77f08e312e10ab3765af4806445cf505536b0e
Balazs M. Fekete <bfekete@ccny.cuny.edu>
2020-10-16 11:25:13 -0400

    Fine tuning parallelization.
    Signed-off-by: Balazs M. Fekete <bfekete@ccny.cuny.edu>

--- CMlib/src/cmThreads.c
+++ CMlib/src/cmThreads.c
@@ -275,8 +275,8 @@ CMthreadTeam_p CMthreadTeamInitialize (CMthreadTeam_p team, size_t threadNum, si
     struct timeval  tp;
     struct timezone tzp;
 
-    if (((taskNum >> 0x000CL) <= 1) || (taskNum > 0x40000000L)) threadNum = 1;
-    else threadNum = (taskNum >> 0x000CL) < threadNum ? (taskNum >> 0x000CL) : threadNum;
+    if ((taskNum >> 0x0100L) <= 1) threadNum = 1;
+    else threadNum = (taskNum >> 0x0100L) < threadNum ? (taskNum >> 0x0100L) : threadNum;
 
     gettimeofday(&tp, &tzp);
     team->TotTime = tp.tv_sec * 1000000 + tp.tv_usec;
