fbed94b72b4a91c4c1223e25064bafb87756a945
Balazs M. Fekete <bfekete@ccny.cuny.edu>
2020-10-22 19:45:52 -0400

    Remove test "tee".
    Signed-off-by: Balazs M. Fekete <bfekete@ccny.cuny.edu>

--- Scripts/rgis2gpkg.sh
+++ Scripts/rgis2gpkg.sh
@@ -130,7 +130,7 @@ case "${EXTENSION}" in
 		gdal_polygonize.py -8 "${TEMPFILE}.tif" -f "ESRI Shapefile" "${TEMPFILE}.shp"
 		ogr2ogr ${UPDATEFLAG} -a_srs EPSG:4326 -f "GPKG" -nln "${SCHEMA}_${TBLNAME}_geom" -nlt PROMOTE_TO_MULTI "${GEOPACKAGE}" "${TEMPFILE}.shp"
 		rgis2sql -c "${CASE}" -a "DBItems" -s "${SCHEMA}" -q "${TBLNAME}" -d "sqlite" -r off "${RGISFILE}" | spatialite -silent "${GEOPACKAGE}"
-		_GPKGattribTable "${SCHEMA}" "${TBLNAME}" "POLYGON" "${ID}" "DN" | tee test.sql | spatialite -silent "${GEOPACKAGE}"
+		_GPKGattribTable "${SCHEMA}" "${TBLNAME}" "POLYGON" "${ID}" "DN" | spatialite -silent "${GEOPACKAGE}"
         rm "${TEMPFILE}".*
 	;;
 	(gdbc|gdbc.gz|nc)
