b4b7d0241623d9fdbe39a7e4a024f4be38a86963
Balazs M. Fekete <bfekete@ccny.cuny.edu>
2020-10-22 20:52:36 -0400

    Tryng SpatiaLite batch mode.
    Signed-off-by: Balazs M. Fekete <bfekete@ccny.cuny.edu>

--- Scripts/rgis2gpkg.sh
+++ Scripts/rgis2gpkg.sh
@@ -39,21 +39,21 @@ function _GPKGattribTable () {
 	local   relateID="${1}"; shift
 	local     joinID="${1}"; shift
 
-	echo -n " INSERT INTO \"gpkg_contents\""
-	echo -n " SELECT \"${schemaName}_${tableName}\", \"data_type\", \"${schemaName}_${tableName}\", \"description\", \"last_change\", \"min_x\", \"min_y\", \"max_x\", \"max_y\", \"srs_id\""
-	echo -n " FROM "gpkg_contents" WHERE "table_name" = \"${schemaName}_${tableName}_geom\";"
-	echo -n " INSERT INTO \"gpkg_ogr_contents\""
-	echo -n " SELECT \"${schemaName}_${tableName}\", \"feature_count\" FROM "gpkg_ogr_contents" WHERE "table_name" = \"${schemaName}_${tableName}_geom\";"
-	echo -n " SELECT gpkgAddGeometryColumn(\"${schemaName}_${tableName}\", \"geom\", '${dataType}', 0, 0, 4326);"
-	echo -n " UPDATE \"${schemaName}_${tableName}\""
-	echo -n " SET \"geom\" = (SELECT \"${schemaName}_${tableName}_geom\".\"geom\""
- 	echo -n " FROM \"${schemaName}_${tableName}_geom\""
-	echo -n " WHERE \"${schemaName}_${tableName}\".\"${relateID}\" = \"${schemaName}_${tableName}_geom\".\"${joinID}\");"
-	echo -n " DROP TABLE \"${schemaName}_${TBLNAME}_geom\";"
-	echo -n " DELETE FROM \"gpkg_metadata_reference\" WHERE \"table_name\" = \"${schemaName}_${tableName}_geom\";"
-	echo -n " DELETE FROM \"gpkg_geometry_columns\"   WHERE \"table_name\" = \"${schemaName}_${tableName}_geom\";"
-	echo -n " DELETE FROM \"gpkg_contents\"           WHERE \"table_name\" = \"${schemaName}_${tableName}_geom\";"
-	echo -n " DELETE FROM \"gpkg_ogr_contents\"       WHERE \"table_name\" = \"${schemaName}_${tableName}_geom\";"
+	echo "INSERT INTO \"gpkg_contents\""
+	echo "SELECT \"${schemaName}_${tableName}\", \"data_type\", \"${schemaName}_${tableName}\", \"description\", \"last_change\", \"min_x\", \"min_y\", \"max_x\", \"max_y\", \"srs_id\""
+	echo "FROM "gpkg_contents" WHERE "table_name" = \"${schemaName}_${tableName}_geom\";"
+	echo "INSERT INTO \"gpkg_ogr_contents\""
+	echo "SELECT \"${schemaName}_${tableName}\", \"feature_count\" FROM "gpkg_ogr_contents" WHERE "table_name" = \"${schemaName}_${tableName}_geom\";"
+	echo "SELECT gpkgAddGeometryColumn(\"${schemaName}_${tableName}\", \"geom\", '${dataType}', 0, 0, 4326);"
+	echo "UPDATE \"${schemaName}_${tableName}\""
+	echo "SET \"geom\" = (SELECT \"${schemaName}_${tableName}_geom\".\"geom\""
+ 	echo "                FROM \"${schemaName}_${tableName}_geom\""
+	echo "                WHERE \"${schemaName}_${tableName}\".\"${relateID}\" = \"${schemaName}_${tableName}_geom\".\"${joinID}\");"
+	echo "DROP TABLE \"${schemaName}_${TBLNAME}_geom\";"
+	echo "DELETE FROM \"gpkg_metadata_reference\" WHERE \"table_name\" = \"${schemaName}_${tableName}_geom\";"
+	echo "DELETE FROM \"gpkg_geometry_columns\"   WHERE \"table_name\" = \"${schemaName}_${tableName}_geom\";"
+	echo "DELETE FROM \"gpkg_contents\"           WHERE \"table_name\" = \"${schemaName}_${tableName}_geom\";"
+	echo "DELETE FROM \"gpkg_ogr_contents\"       WHERE \"table_name\" = \"${schemaName}_${tableName}_geom\";"
 }
 
 while [ "${1}" != "" ]
@@ -113,15 +113,15 @@ TEMPFILE="$(mktemp -u -t rgis2gpkgXXXX)"
  
 case "${EXTENSION}" in
 	(gdbt)
-		rgis2sql -c "${CASE}" -a "DBItems" -s "${SCHEMA}" -q "${TBLNAME}" -d "sqlite" -r off "${RGISFILE}" | spatialite  -silent "${GEOPACKAGE}"
+		rgis2sql -c "${CASE}" -a "DBItems" -s "${SCHEMA}" -q "${TBLNAME}" -d "sqlite" -r off "${RGISFILE}" | spatialite -batch  "${GEOPACKAGE}"
 	;;
 	(gdbp|gdbp.gz|gdbl|gdbl.gz)
 		[ "${EXTENSION%.gz}" == gdbp ] && DATATYPE="POINT" || DATATYPE="LINESTRING"
 		rgis2ascii "${RGISFILE}" "${TEMPFILE}.asc"
 		ogr2ogr -a_srs EPSG:4326 -f "ESRI Shapefile" "${TEMPFILE}.shp" "${TEMPFILE}.asc"
 		ogr2ogr ${UPDATEFLAG} -a_srs EPSG:4326 -f "GPKG" -nln "${SCHEMA}_${TBLNAME}_geom" "${GEOPACKAGE}" "${TEMPFILE}.shp"
-		rgis2sql -c "${CASE}" -a "DBItems" -s "${SCHEMA}" -q "${TBLNAME}" -d "sqlite" -r off "${RGISFILE}" | spatialite  -silent "${GEOPACKAGE}"
-		_GPKGattribTable "${SCHEMA}" "${TBLNAME}" "${DATATYPE}" "${ID}" "fid" | spatialite -silent "${GEOPACKAGE}"
+		rgis2sql -c "${CASE}" -a "DBItems" -s "${SCHEMA}" -q "${TBLNAME}" -d "sqlite" -r off "${RGISFILE}" | spatialite -batch  "${GEOPACKAGE}"
+		_GPKGattribTable "${SCHEMA}" "${TBLNAME}" "${DATATYPE}" "${ID}" "fid" | spatialite -batch  "${GEOPACKAGE}"
 		rm "${TEMPFILE}".*
  	;;
 	(gdbd|gdbd.gz)
@@ -129,8 +129,8 @@ case "${EXTENSION}" in
 		gdal_translate -a_srs EPSG:4326 "${TEMPFILE}.grd" "${TEMPFILE}.tif"
 		gdal_polygonize.py -8 "${TEMPFILE}.tif" -f "ESRI Shapefile" "${TEMPFILE}.shp"
 		ogr2ogr ${UPDATEFLAG} -a_srs EPSG:4326 -f "GPKG" -nln "${SCHEMA}_${TBLNAME}_geom" -nlt PROMOTE_TO_MULTI "${GEOPACKAGE}" "${TEMPFILE}.shp"
-		rgis2sql -c "${CASE}" -a "DBItems" -s "${SCHEMA}" -q "${TBLNAME}" -d "sqlite" -r off "${RGISFILE}" | spatialite -silent "${GEOPACKAGE}"
-		_GPKGattribTable "${SCHEMA}" "${TBLNAME}" "POLYGON" "${ID}" "DN" | spatialite -silent "${GEOPACKAGE}"
+		rgis2sql -c "${CASE}" -a "DBItems" -s "${SCHEMA}" -q "${TBLNAME}" -d "sqlite" -r off "${RGISFILE}" | spatialite -batch  "${GEOPACKAGE}"
+		_GPKGattribTable "${SCHEMA}" "${TBLNAME}" "POLYGON" "${ID}" "DN" | spatialite -batch  "${GEOPACKAGE}"
         rm "${TEMPFILE}".*
 	;;
 	(gdbc|gdbc.gz|nc)
