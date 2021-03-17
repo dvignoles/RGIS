a404c3fb33eb3add2268f0d84873c494c9ea8e58
Balazs M. Fekete <bfekete@ccny.cuny.edu>
2021-03-16 20:34:27 -0400

    Commenting out changes.
    Signed-off-by: Balazs M. Fekete <bfekete@ccny.cuny.edu>

--- Scripts/rgis2GPKG.sh
+++ Scripts/rgis2GPKG.sh
@@ -48,19 +48,19 @@ function _GPKGattribTable () {
 	echo "SELECT \"${schemaName}_${tableName}\", \"feature_count\" FROM "gpkg_ogr_contents" WHERE "table_name" = \"${schemaName}_${tableName}_geom\";"
 	echo "SELECT gpkgAddGeometryColumn(\"${schemaName}_${tableName}\", \"geom\", '${dataType}', 0, 0, 4326);"
 	echo "UPDATE \"${schemaName}_${tableName}\""
-	if [[ "${dataType}" == "POLYGON" ]]
-	then
-		echo "SET \"geom\" = (SELECT \"geom_table\".\"geom\""
- 		echo "                FROM (SELECT \"${joinID}\" AS \"${joinID}\","
-		echo "                             ST_BUFFER (ST_UNION (\"${schemaName}_${tableName}_geom\".\"geom\"),0.0) AS \"geom\""
-		echo "                      FROM     \"${schemaName}_${tableName}_geom\""
-		echo "                      GROUP BY \"${schemaName}_${tableName}_geom\".\"${joinID}\") AS \"geom_table\""
-		echo "                WHERE \"${schemaName}_${tableName}\".\"${relateID}\" = \"geom_table\".\"${joinID}\");"
-	else
+#	if [[ "${dataType}" == "POLYGON" ]]
+#	then
+#		echo "SET \"geom\" = (SELECT \"geom_table\".\"geom\""
+# 		echo "                FROM (SELECT \"${joinID}\" AS \"${joinID}\","
+#		echo "                             ST_BUFFER (ST_UNION (\"${schemaName}_${tableName}_geom\".\"geom\"),0.0) AS \"geom\""
+#		echo "                      FROM     \"${schemaName}_${tableName}_geom\""
+#		echo "                      GROUP BY \"${schemaName}_${tableName}_geom\".\"${joinID}\") AS \"geom_table\""
+#		echo "                WHERE \"${schemaName}_${tableName}\".\"${relateID}\" = \"geom_table\".\"${joinID}\");"
+#	else
 		echo "SET \"geom\" = (SELECT \"${schemaName}_${tableName}_geom\".\"geom\""
  		echo "                FROM \"${schemaName}_${tableName}_geom\""
 		echo "                WHERE \"${schemaName}_${tableName}\".\"${relateID}\" = \"${schemaName}_${tableName}_geom\".\"${joinID}\");"
-	fi
+#	fi
 	echo "DROP TABLE \"${schemaName}_${TBLNAME}_geom\";"
 	echo "DELETE FROM \"gpkg_metadata_reference\" WHERE \"table_name\" = \"${schemaName}_${tableName}_geom\";"
 	echo "DELETE FROM \"gpkg_geometry_columns\"   WHERE \"table_name\" = \"${schemaName}_${tableName}_geom\";"
