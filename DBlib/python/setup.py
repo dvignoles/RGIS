98602e0aabe18aeb296b7e93934e486cbe68f21d
Daniel Vignoles <dvignoles@gmail.com>
2021-04-16 11:15:38 -0400

    python swig setup

---
+++ DBlib/python/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+"""
+setup.py file for SWIG example
+"""
+
+from distutils.core import setup, Extension
+
+
+example_module = Extension('_pyDBlib',
+                           sources=['pyDBlib/dblibPYTHON_wrap.cxx','/home/dan/Dev/RGIS/DBlib/src/DBImpExpNetCDF.C'],
+                           include_dirs = ['/usr/local/ghaas/include',
+                           '/home/dan/Dev/RGIS/CMlib/include',
+                           '/usr/include'
+                           ],
+                           )
+
+setup (name = 'pyDBlib',
+       version = '0.1',
+       author      = "SWIG Docs",
+       description = """Simple swig example from docs""",
+       ext_modules = [example_module],
+       #py_modules = ["pyDBlib"],
+       )
